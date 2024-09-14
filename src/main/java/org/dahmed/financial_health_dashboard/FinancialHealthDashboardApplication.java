b188b692a8cb9de5fba6677b9191fdf971a1a084
dinul.ahmed <dinul.ahmed@fdmgroup.com>
2024-09-14 15:36:51 +0100

    initial commit

---
+++ src/main/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.dahmed.financial_health_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialHealthDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialHealthDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplicationTests.java
@@ -0,0 +1,14 @@
+package org.dahmed.financial_health_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialHealthDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//incomplete implementation
+	}
+
+}
