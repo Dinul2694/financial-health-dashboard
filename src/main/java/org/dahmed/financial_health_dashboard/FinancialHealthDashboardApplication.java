463e428d7e83ed2d26fddd3a936fd669b51867c5
dinul.ahmed <dinulahmed@hotmail.co.uk>
2024-09-15 00:13:17 +0100

    testing pushes

--- src/main/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplication.java
+++ src/main/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinancialHealthDashboardApplication {
 
 	public static void main(String[] args) {
+		//comment
 		SpringApplication.run(FinancialHealthDashboardApplication.class, args);
 	}
 
