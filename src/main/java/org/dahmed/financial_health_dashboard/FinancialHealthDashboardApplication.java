7a58e1ef5cddd96c9e2e930dab30270d8423a60e
dinul.ahmed <dinulahmed@hotmail.co.uk>
2024-09-15 05:46:52 +0100

    remove random comments

--- src/main/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplication.java
+++ src/main/java/org/dahmed/financial_health_dashboard/FinancialHealthDashboardApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinancialHealthDashboardApplication {
 
 	public static void main(String[] args) {
-		//comment 2
 		SpringApplication.run(FinancialHealthDashboardApplication.class, args);
 	}
 
